633ef1fa4647e2c7482124a7037bb6f6e84775ba
Jan Was <jwas@nets.com.pl>
2014-04-10 17:19:11 +0200

    moved a common method to StateTransition model

--- components/StateTransition.php
+++ components/StateTransition.php
@@ -12,6 +12,31 @@ class StateTransition extends CComponent
     public $confirmation_required = false;
     public $display_order;
 
+    /**
+     * Creates a list of StateTransition as all possible combinations between passes states.
+     * States must be objects with following properties: id, pre_label, post_label, icon, css_class
+     * @param $states array
+     * @return array
+     */
+    public static function statesToTransitions($states)
+    {
+        $result = array();
+        for($i = 0; $i < count($states); $i++) {
+            for($j = 0; $j < count($states); $j++) {
+                if ($i == $j) continue;
+                $transition = new StateTransition;
+                $transition->source_state = $states[$i]->id;
+                $transition->target_state = $states[$j]->id;
+                $transition->label = $states[$j]->pre_label;
+                $transition->post_label = $states[$j]->post_label;
+                $transition->icon = $states[$j]->icon;
+                $transition->css_class = $states[$j]->css_class;
+                $result[] = $transition;
+            }
+        }
+        return $result;
+    }
+
     public static function groupBySource($transitions, $source_attribute, $target_attribute)
     {
         $result = array();
