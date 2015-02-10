a91f861a5526b96c2f1dadc5f4ed914c37ad6934
Jan Was <jwas@nets.com.pl>
2015-02-10 15:39:18 +0100

    improve BulkStateAction to match niix.components.BulkUpdateAction

--- components/BulkStateAction.php
+++ components/BulkStateAction.php
@@ -150,21 +150,35 @@ class BulkStateAction extends BaseBulkAction
         $baseModel->{$baseMdel->stateAttributeName} = $this->getSourceState($baseMdel);
         list($stateChange, $sourceState, $uiType) = $this->_stateAction->prepare($baseModel);
 
+        if ($this->singleTransaction) {
+            $trx = $baseModel->dbConnection->currentTransaction === null ? $baseModel->dbConnection->beginTransaction() : null;
+        }
         $dataProvider = $this->getDataProvider($baseModel, $this->getCriteria());
+        $skippedKeys = array();
+        $failedKeys = array();
         foreach($dataProvider->getData() as $model) {
             if (isset($stateChange['state']->auth_item_name) && !Yii::app()->user->checkAccess($stateChange['state']->auth_item_name, array('model'=>$model))) {
-                //! @todo make a list of skipped models
+                $skippedKeys[] = $model->primaryKey;
             }
 
             $model->setTransitionRules($targetState);
             $this->controller->initForm($model);
 
             if (!$this->_stateAction->performTransition($model, $stateChange, $sourceState, $targetState, true)) {
-                //! @todo make a list of failed models
+                //! @todo errors should be gathered and displayed somewhere, maybe add a postSummary action in this class
+                $failedKeys[] = $model->primaryKey;
             }
         }
 
-        //! @todo render something in case of an error
-        $this->redirect(array('index'));
+        if ($trx !== null) {
+            $trx->commit();
+        }
+        $message = Yii::t('app', '{number} out of {total} {model} has been successfully updated.', array(
+            '{number}' => $dataProvider->getTotalItemCount() - count($failedKeys) - count($skippedKeys),
+            '{total}' => $dataProvider->getTotalItemCount(),
+            '{model}' => $baseModel->label($dataProvider->getTotalItemCount()),
+        ));
+        Yii::app()->user->setFlash($this->postFlashKey, $message);
+        $this->controller->redirect(array($this->postRoute));
     }
 }
