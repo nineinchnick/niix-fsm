add49b87dd4d9c07bb2b6d17658d28343b51bb09
Jan Waś <janek.jan@gmail.com>
2015-03-08 18:34:33 +0100

    cleanup

--- commands/FsmCommand.php
+++ commands/FsmCommand.php
@@ -7,36 +7,36 @@ Yii::import('system.cli.commands.MigrateCommand');
  */
 class FsmCommand extends CConsoleCommand
 {
-	/**
-	 * @var string the directory that stores the migrations. This must be specified
-	 * in terms of a path alias, and the corresponding directory must exist.
-	 * Defaults to 'application.migrations' (meaning 'protected/migrations').
-	 */
-	public $migrationPath='application.migrations';
-	/**
-	 * @var string the path of the template file for generating new migrations. This
-	 * must be specified in terms of a path alias (e.g. application.migrations.template).
-	 * If not set, an internal template will be used.
-	 */
-	public $templateFile='fsm.migrations.template';
-	/**
-	 * @var boolean whether to execute the migration in an interactive mode. Defaults to true.
-	 * Set this to false when performing migration in a cron job or background process.
-	 */
-	public $interactive=true;
+    /**
+     * @var string the directory that stores the migrations. This must be specified
+     * in terms of a path alias, and the corresponding directory must exist.
+     * Defaults to 'application.migrations' (meaning 'protected/migrations').
+     */
+    public $migrationPath='application.migrations';
+    /**
+     * @var string the path of the template file for generating new migrations. This
+     * must be specified in terms of a path alias (e.g. application.migrations.template).
+     * If not set, an internal template will be used.
+     */
+    public $templateFile='fsm.migrations.template';
+    /**
+     * @var boolean whether to execute the migration in an interactive mode. Defaults to true.
+     * Set this to false when performing migration in a cron job or background process.
+     */
+    public $interactive=true;
 
-	public function beforeAction($action,$params)
-	{
-		$path=Yii::getPathOfAlias($this->migrationPath);
-		if($path===false || !is_dir($path))
-		{
-			echo 'Error: The migration directory does not exist: '.$this->migrationPath."\n";
-			exit(1);
-		}
-		$this->migrationPath=$path;
+    public function beforeAction($action,$params)
+    {
+        $path=Yii::getPathOfAlias($this->migrationPath);
+        if($path===false || !is_dir($path))
+        {
+            echo 'Error: The migration directory does not exist: '.$this->migrationPath."\n";
+            exit(1);
+        }
+        $this->migrationPath=$path;
 
-		return parent::beforeAction($action,$params);
-	}
+        return parent::beforeAction($action,$params);
+    }
 
     /**
      * Changes:
@@ -46,8 +46,8 @@ class FsmCommand extends CConsoleCommand
      * @param $relation string name of relation from model pointing to the main model, ex. orders
      * @param $schema
      */
-	public function actionCreate($modelClass, $tableSuffix, $relation, $schema='')
-	{
+    public function actionCreate($modelClass, $tableSuffix, $relation, $schema='')
+    {
         if (($pos=strrpos($modelClass,'.'))!==false) {
             Yii::import($modelClass);
             $modelClass = substr($modelClass,$pos+1);
@@ -58,7 +58,7 @@ class FsmCommand extends CConsoleCommand
 
         Yii::import('niix.helpers.InflectorHelper');
 
-		$name='m'.gmdate('ymd_His').'_install_fsm';
+        $name='m'.gmdate('ymd_His').'_install_fsm';
         $content=strtr($this->getTemplate(), array(
             '{ClassName}'   => $name,
             '{TableName}'   => $model->tableName(),
@@ -69,24 +69,24 @@ class FsmCommand extends CConsoleCommand
             '{MainTableName}'  => $mainModel->tableName(),
             '{MainPrimaryKey}'  => $mainModel->tableSchema->primaryKey,
         ));
-		$file=$this->migrationPath.DIRECTORY_SEPARATOR.$name.'.php';
+        $file=$this->migrationPath.DIRECTORY_SEPARATOR.$name.'.php';
 
-		if($this->confirm("Create new migration '$file'?"))
-		{
-			file_put_contents($file, $content);
-			echo "New migration created successfully.\n";
-		}
-	}
+        if($this->confirm("Create new migration '$file'?"))
+        {
+            file_put_contents($file, $content);
+            echo "New migration created successfully.\n";
+        }
+    }
 
-	public function confirm($message,$default=false)
-	{
-		if(!$this->interactive)
-			return true;
-		return parent::confirm($message,$default);
-	}
+    public function confirm($message,$default=false)
+    {
+        if(!$this->interactive)
+            return true;
+        return parent::confirm($message,$default);
+    }
 
-	protected function getTemplate()
-	{
-		return file_get_contents(Yii::getPathOfAlias($this->templateFile).'.php');
-	}
+    protected function getTemplate()
+    {
+        return file_get_contents(Yii::getPathOfAlias($this->templateFile).'.php');
+    }
 }
